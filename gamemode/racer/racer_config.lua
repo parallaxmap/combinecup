b5d91a9c7c64446520b7b99a0330788fc9c6f1b4
parallaxmap <landpondskater@proton.me>
2026-03-15 14:39:39 +0000

    pseudorandom player appearance based on steamid

--- gamemode/racer/racer_config.lua
+++ gamemode/racer/racer_config.lua
@@ -3,7 +3,44 @@ function GM:PlayerSpawn(ply)
         player_manager.SetPlayerClass(ply, "player_default")
     end
 
-	ply:SetModel("models/player/group01/male_07.mdl")
+	local models = {
+		"male_07", 
+		"breen",
+		"alyx",
+		"gman_high",
+		"barney",
+		"police", 
+		"combine_soldier",
+		"combine_super_soldier",
+		"zombie_soldier",
+
+		"group03/male_01",
+		"group03/male_02",
+		"group03/male_03",
+		"group03/male_04",
+		"group03/male_05",
+		"group03/male_06",
+		"group03/male_07",
+		"group03/male_08",
+		"group03/male_09",
+
+		"group03/female_01",
+		"group03/female_02",
+		"group03/female_03",
+		"group03/female_04",
+		"group03/female_05",
+		"group03/female_06",
+	}
+
+	local model = "models/player/" .. table.Random(models) .. ".mdl"
+
+	ply:SetModel(model)
+
+	local r = math.random(0, 255) / 255
+	local g = math.random(0, 255) / 255
+	local b = math.random(0, 255) / 255
+
+	ply:SetPlayerColor( Vector( r, g, b ) )
 	ply:SetupHands()
 
 	ply:SetNWInt("CurrentCheckpoint", ply:GetNWInt("CurrentCheckpoint", 0))
@@ -13,3 +50,32 @@ function GM:PlayerSpawn(ply)
     
     ply:SpawnAtSpecificCheckpointEnt()
 end
+
+hook.Add("PlayerInitialSpawn", "PlayerRandomAppearance", function(ply)
+    local models = {
+        "breen", "alyx", "gman_high", "barney", "police", 
+        "combine_soldier", "combine_super_soldier", "zombie_soldier",
+        "group03/male_01", "group03/male_02", "group03/male_03",
+        "group03/male_04", "group03/male_05", "group03/male_06",
+        "group03/male_07", "group03/male_08", "group03/male_09",
+        "group03/female_01", "group03/female_02", "group03/female_03",
+        "group03/female_04", "group03/female_05", "group03/female_06",
+    }
+
+    local id = ply:SteamID()
+
+    local modelIndex = math.floor(util.SharedRandom("playermodel", 1, #models, id))
+    local randomModel = models[modelIndex]
+
+    local r = util.SharedRandom("playercolor_r", 0, 1, id)
+    local g = util.SharedRandom("playercolor_g", 0, 1, id)
+    local b = util.SharedRandom("playercolor_b", 0, 1, id)
+
+    timer.Simple(0, function()
+        if not IsValid(ply) then return end
+        
+        ply:SetModel("models/player/" .. randomModel .. ".mdl")
+        ply:SetPlayerColor(Vector(r, g, b))
+        ply:SetupHands()
+    end)
+end)
